2c5968ae0665e90f6903599717bf85631c11e2d0
Nahum28 <luis_nahum28@hotmail.com>
2025-04-21 20:37:08 -0600

    creacion de commons

---
+++ commons/src/main/java/com/commons/services/CommonsService.java
@@ -0,0 +1,17 @@
+package com.commons.services;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommonsService<D> {
+
+	List<D> listar();
+	
+	Optional<D> obtenerPorId(Long id);
+	
+	D insertar(D dto);
+	
+	D editar(D dto , Long id);
+	
+	D eliminar(Long id);
+}
